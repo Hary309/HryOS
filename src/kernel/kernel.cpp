95f159a272459e35d7f5c9001d05205be6d2c7af
Piotr Krupa <piotrkrupa06@gmail.com>
2020-10-28 10:58:20 +0100

    Add magic and multiboot info to kernel_main

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,12 +1,20 @@
+#include <stdint.h>
+
 #include "terminal/color.hpp"
 #include "terminal/terminal.hpp"
 
 #include "multiboot.h"
 
-extern "C" void kernel_main()
+extern "C" void kernel_main(uint32_t magic, multiboot_info* info)
 {
     terminal::init();
     terminal::clear_screen();
+
+    if (magic != MULTIBOOT_BOOTLOADER_MAGIC)
+    {
+        terminal::print_line("Invalid magic number");
+        return;
+    }
 
     terminal::print("Hry");
     terminal::set_foreground_color(terminal::color::cyan);
