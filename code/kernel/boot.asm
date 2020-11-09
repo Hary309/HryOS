c2384e7bc87e7f3c8527d2dac5868d3cb0ca36e2
Piotr Krupa <piotrkrupa06@gmail.com>
2020-11-09 23:55:07 +0100

    Remove disabling interrupts before loop

--- code/kernel/boot.asm
+++ code/kernel/boot.asm
@@ -93,7 +93,6 @@ _start:
 	;    Since they are disabled, this will lock up the computer.
 	; 3) Jump to the hlt instruction if it ever wakes up due to a
 	;    non-maskable interrupt occurring or due to system management mode.
-	cli
 .hang:
 	hlt
 	jmp .hang
