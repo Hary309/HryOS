97671b0614d20e0d38de9147606c118179547a36
Piotr Krupa <piotrkrupa06@gmail.com>
2020-12-13 19:18:50 +0100

    Add circular buffer

---
+++ code/hlib/include/circular_buffer.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "array.hpp"
+#include "optional.hpp"
+
+namespace hlib
+{
+    template<typename T, size_t Size>
+    class circular_buffer
+    {
+    public:
+        using value_type = T;
+        using iterator = value_type*;
+        using const_iterator = const value_type*;
+        using size_type = size_t;
+
+    public:
+        constexpr void push(const value_type& data)
+        {
+            if (!full())
+            {
+                buffer_[tail_] = data;
+                tail_ = (tail_ + 1) % Size;
+            }
+        }
+
+        constexpr hlib::optional<value_type> pop()
+        {
+            if (!empty())
+            {
+                value_type data = buffer_[head_];
+
+                buffer_[head_] = value_type();
+
+                head_ = (head_ + 1) % Size;
+
+                return data;
+            }
+
+            return {};
+        }
+
+        constexpr iterator front()
+        {
+            return buffer_[head_];
+        }
+
+        constexpr const_iterator front() const
+        {
+            return buffer_[head_];
+        }
+
+        constexpr iterator back()
+        {
+            return buffer_[tail_];
+        }
+
+        constexpr const_iterator back() const
+        {
+            return buffer_[tail_];
+        }
+
+        constexpr T* data()
+        {
+            return buffer_.data();
+        }
+
+        constexpr bool empty()
+        {
+            return head_ == tail_;
+        }
+
+        constexpr bool full()
+        {
+            return tail_ == (head_ - 1) % Size;
+        }
+
+    private:
+        size_t head_ = 0;
+        size_t tail_ = 0;
+        hlib::array<T, Size> buffer_;
+    };
+} // namespace hlib
